2f29af3bce7c4e93d57021154ed1cd73613d25c8
Adeshen <878600556@qq.com>
2022-07-03 23:50:00 +0800

    all ok

--- ipRevolution.py
+++ ipRevolution.py
@@ -23,24 +23,23 @@ def restoreIpAddresses(s):
     ans=[]
     for i in range(1,t):
         if((s[0]=='0' and i>1) or i > 3 ):
-            print(1)
+            # print(1)
             break
         if(intfun(s[0:i])> 255 ):
-            print(1.0,intfun(s[0:i]))
+            # print(1.0,intfun(s[0:i]))
             break
         for j in range(i+1,t):
             if((s[i]=='0' and j>i+1) or j>i+3):
-                print(2)
+                # print(2)
                 break
             if( intfun(s[i:j]) >255):
-                print(2.)
+                # print(2.)
                 break
             for k in range(j+1,t):
                 if((s[j]=='0' and k>j+1) or k > j+3):
                     break
                 if(intfun(s[j:k])>255):
                     break
-                
                 if(s[k]=='0' and k<=t-2):
                     continue
                 if(t-k>3):
@@ -49,9 +48,9 @@ def restoreIpAddresses(s):
                     continue
                 ans.append(s[0:i]+"."+s[i:j]+"."+s[j:k]+"."+s[k:t])
     return ans
-
-print(intfun("123456"[0:2]))
-print(int("123"))
+# print(intfun("123456"[0:2]))
+# print(int("123"))
 print(restoreIpAddresses("25525511135"))    
-
+print(restoreIpAddresses("0000"))
+print(restoreIpAddresses("101023"))
 print(restoreIpAddresses("172162541"))
