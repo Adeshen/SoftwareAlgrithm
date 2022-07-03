243e72fddab51fcf723882fd12671d2b8f1dfd0f
Adeshen <878600556@qq.com>
2022-07-03 23:45:40 +0800

    bug over

--- ipRevolution.py
+++ ipRevolution.py
@@ -3,12 +3,12 @@
 # 参考leetcode格式
 # from asyncio.windows_events import NULL
 def intfun(s):
-    temp=0
-    for i in s:
-        temp*=10
-        temp+=int(i)
-    return temp
-
+    # temp=0
+    # for i in s:
+    #     temp*=10
+    #     temp+=int(i)
+    # return temp
+    return int(s)
 
 def restoreIpAddresses(s):
     """
@@ -29,21 +29,24 @@ def restoreIpAddresses(s):
             print(1.0,intfun(s[0:i]))
             break
         for j in range(i+1,t):
-            if((s[i]=='0' and j>i) or j>i+3):
+            if((s[i]=='0' and j>i+1) or j>i+3):
                 print(2)
                 break
             if( intfun(s[i:j]) >255):
                 print(2.)
                 break
             for k in range(j+1,t):
-                if((s[j]=='0' and k>j) or k > j+3):
+                if((s[j]=='0' and k>j+1) or k > j+3):
                     break
                 if(intfun(s[j:k])>255):
                     break
+                
+                if(s[k]=='0' and k<=t-2):
+                    continue
                 if(t-k>3):
                     continue
                 if(intfun(s[k:t])>255):
-                    break
+                    continue
                 ans.append(s[0:i]+"."+s[i:j]+"."+s[j:k]+"."+s[k:t])
     return ans
 
@@ -51,4 +54,4 @@ print(intfun("123456"[0:2]))
 print(int("123"))
 print(restoreIpAddresses("25525511135"))    
 
-print(restoreIpAddresses("0000"))
+print(restoreIpAddresses("172162541"))
